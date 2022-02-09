132068c435b6554cb89b1b28aa7de8c4472fd9f9
lauras521 <laurastock521@gmail.com>
2022-02-08 22:30:19 -0600

    started code for challenge

--- static/js/app.js
+++ static/js/app.js
@@ -1,40 +1,66 @@
-//import the data from data.js
-const tableData=data;
-//reference the HTML table using D3 - JS library that adds interactive functionality (i.e. listening for clicks)
+// from data.js
+const tableData = data;
+
+// get table references
 var tbody = d3.select("tbody");
 
 function buildTable(data) {
-    //tells JS to use an empty string when creating the table
-    tbody.html("");
-    //loop through each object in data and append a row and cells for each value in the row
-    data.forEach((dataRow) => {
-        //add a row to the table body
-        let row=tbody.append('tr');
-        //loop through each field in the dataRow and add each value as a table cell (td)
-        Object.values(dataRow).forEach((val)=> {
-            let cell = row.append('td');
-            cell.text(val);
-            }
-        );
+  // First, clear out any existing data
+  tbody.html("");
+
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
     });
+  });
 }
-function handleClick(){
-    // tells d3 to look for #datetime id in HTML tags, grab information and store it in date variable
-    let date = d3.select("#datetime").property('value');
-    //set a default filter and save it to a new variable
-    let filteredData=tableData;
-    //show only rows where date = t date filter created 
-    //if statement syntax   if(condition){code to execute}
-    // pseudocode practice if (a date is entered) {Filter the default data to show only the date entered};
-    if(date){
-        filteredData=filteredData.filter(row=>row.datetime===date);
-    } //had ; here earlier in module and then they take it out at the bottom of 11.5.4
-    //build table using f iltered data
-    buildTable(filteredData)};
 
-//listen for button click
-d3.selectAll('#filter-btn').on('click',handleClick);
+// 1. Create a variable to keep track of all the filters as an object.
 
 
-//build table when page loads
-buildTable(tableData);
+// 3. Use this function to update the filters. 
+function updateFilters() {
+
+    // 4a. Save the element that was changed as a variable.
+
+    // 4b. Save the value that was changed as a variable.
+
+    // 4c. Save the id of the filter that was changed as a variable.
+
+  
+    // 5. If a filter value was entered then add that filterId and value
+    // to the filters list. Otherwise, clear that filter from the filters object.
+ 
+  
+    // 6. Call function to apply all filters and rebuild the table
+    filterTable();
+  
+  }
+  
+  // 7. Use this function to filter the table when data is entered.
+  function filterTable() {
+  
+    // 8. Set the filtered data to the tableData.
+    
+  
+    // 9. Loop through all of the filters and keep any data that
+    // matches the filter values
+    
+  
+    // 10. Finally, rebuild the table using the filtered data
+    
+  }
+  
+  // 2. Attach an event to listen for changes to each filter
+  
+  
+  // Build the table when the page loads
+  buildTable(tableData);
